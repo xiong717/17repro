f67b61e443e4d241fd4f82d0877d99b2217a110c
xiong717 <1304158615@qq.com>
2021-01-26 19:06:00 +0800

    二叉树遍历 结点个数 求高度

---
+++ 20210126二叉树下（遍历）/testDemo.java
@@ -0,0 +1,27 @@
+public class testDemo {
+    public static void main(String[] args) {
+        BinaryTree binaryTree = new BinaryTree();
+        BTNode root = binaryTree.createTree();
+        binaryTree.preOrderTraversal(root);
+        System.out.println();
+        binaryTree.inOrderTraversal(root);
+        System.out.println();
+        binaryTree.postOrderTraversal(root);
+        System.out.println();
+
+        binaryTree.getSize1(root);
+        System.out.println(binaryTree.size);
+
+        System.out.println(binaryTree.getSize2(root));
+
+        binaryTree.getLeafSize1(root);
+        System.out.println(binaryTree.leafSize);
+
+        System.out.println(binaryTree.getLeafSize2(root));
+
+        System.out.println(binaryTree.getKLevelSize(root,4));
+
+        System.out.println(binaryTree.getHeight(root));
+    }
+
+}
