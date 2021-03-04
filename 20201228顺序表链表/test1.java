406a0211cc664e9cb5ec0cce02c0012637c6f752
xiong717 <1304158615@qq.com>
2021-03-04 23:26:15 +0800

    顺序表

---
+++ 20201228顺序表链表/test1.java
@@ -0,0 +1,17 @@
+public class test1 {
+    public static void main(String[] args) {
+        myArraylist p1 = new myArraylist();
+        p1.add(0,6);
+        p1.add(1,66);
+        p1.add(2,666);
+        p1.add(3,888);
+        p1.display();
+        System.out.println(p1.size());
+        System.out.println(p1.contains(66));
+        System.out.println(p1.research(666));
+    }
+
+
+
+
+}
