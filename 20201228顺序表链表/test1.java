90293fed4420d788e18fdc0b20f04ca5c64a12ec
xiong717 <1304158615@qq.com>
2021-03-05 09:49:27 +0800

    顺序表操作 打印 增删改查 清空顺序表

--- 20201228顺序表链表/test1.java
+++ 20201228顺序表链表/test1.java
@@ -5,13 +5,19 @@ public class test1 {
         p1.add(1,66);
         p1.add(2,666);
         p1.add(3,888);
+        p1.add2(999);
         p1.display();
         System.out.println(p1.size());
-        System.out.println(p1.contains(66));
+        System.out.println(p1.contains(666));
         System.out.println(p1.research(666));
+
+        System.out.println(p1.getpos(3));
+
+        p1.setpos(2,444);
+        p1.display();
+        p1.remove(444);
+        p1.display();
+        p1.clear();
+        p1.display();
     }
-
-
-
-
 }
