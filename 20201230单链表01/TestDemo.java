9952ac0945ff4a7584d6f53684a328ee3619bdd1
xiong717 <1304158615@qq.com>
2021-03-06 23:32:13 +0800

    单链表

--- 20201230单链表01/TestDemo.java
+++ 20201230单链表01/TestDemo.java
@@ -15,9 +15,16 @@ public class TestDemo {
         ////这个函数结束之后，我们拿到了当前链表的头节点
         myLinkList.createLinked();
         myLinkList.display();
+        System.out.println("====================");
         Node ret = myLinkList.findLastNode();
         System.out.println(ret.val);
+        System.out.println("====================");
         Node ret1 = myLinkList.findLastTwoNode();
         System.out.println(ret1.val);
+        System.out.println("====================");
+        Node ret2 = myLinkList.findn(1);
+        System.out.println(ret2.val);
+        System.out.println("=====================");
+        System.out.println(myLinkList.contains(422));
     }
 }
