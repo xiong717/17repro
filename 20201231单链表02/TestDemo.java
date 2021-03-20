c308ae4a2cfcd413ea015ac9c4589a413b32a23c
xiong717 <1304158615@qq.com>
2021-03-21 00:11:01 +0800

    翻转链表 合并两个有序链表

--- 20201231单链表02/TestDemo.java
+++ 20201231单链表02/TestDemo.java
@@ -1,5 +1,5 @@
 public class TestDemo {
-    public static void main(String[] args) {
+    public static void main1(String[] args) {
         MyLinkedList myLinkedList = new MyLinkedList();
         myLinkedList.addList(19);
         myLinkedList.addList(29);
@@ -12,5 +12,30 @@ public class TestDemo {
         System.out.println(myLinkedList.head.val);
         Node newhead = myLinkedList.reviseList(myLinkedList.head);
         myLinkedList.display(newhead);
+    }
+
+    public static void main(String[] args) {
+        Node node1 = new Node(12);
+        Node node2 = new Node(22);
+        Node node3 = new Node(27);
+        node1.next = node2;
+        node2.next = node3;
+
+        Node nodel1 = new Node(14);
+        Node nodel2 = new Node(20);
+        Node nodel3 = new Node(25);
+        Node nodel4 = new Node(30);
+        Node nodel5 = new Node(39);
+        nodel1.next = nodel2;
+        nodel2.next = nodel3;
+        nodel3.next = nodel4;
+        nodel4.next = nodel5;
+
+        MyLinkedList myLinkedList = new MyLinkedList();
+
+        Node w = myLinkedList.addtwoLists(node1,nodel1);
+        myLinkedList.display(w);
+
+
     }
 }
