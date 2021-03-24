af420ff69b25b04d246b831b46a7fcbbb81a544f
xiong717 <1304158615@qq.com>
2021-03-24 21:27:09 +0800

    找公共节点

--- 20211231单链表02/TestDemo.java
+++ 20211231单链表02/TestDemo.java
@@ -14,7 +14,7 @@ public class TestDemo {
         myLinkedList.display(newhead);
     }
 
-    public static void main(String[] args) {
+    public static void main2(String[] args) {
         Node node1 = new Node(12);
         Node node2 = new Node(22);
         Node node3 = new Node(27);
@@ -35,7 +35,40 @@ public class TestDemo {
 
         Node w = myLinkedList.addtwoLists(node1,nodel1);
         myLinkedList.display(w);
-
-
+    }
+    public Node getIntersectionNode(Node headA, Node headB) {
+        if (headA == null || headB == null) {
+            return null;
+        }
+        Node pl = headA;//pl 永远指向最长的链表
+        Node ps = headB;//ps 永远指向最短的链表
+        int lenA = 0;
+        int lenB = 0;
+        while (pl != null) {
+            headA = headA.next;
+            lenA++;
+        }
+        while (headB != null) {
+            headB = headB.next;
+            lenB++;
+        }
+        //计算完链表长度 一定要把头结点指回去
+        pl = headA;
+        ps = headB;
+        int len = lenA - lenB;
+        if (len < 0) {
+            pl = headB;
+            ps = headA;
+            len = lenB - lenA;
+        }
+        while (len != 0) {
+            pl = pl.next;
+            len--;
+        }
+        while (pl != ps) {
+            pl = pl.next;
+            ps = ps.next;
+        }
+        return pl;
     }
 }
