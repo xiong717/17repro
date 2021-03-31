8372aa11c6dff25a390a7f2b57228893a17dc174
xiong717 <1304158615@qq.com>
2021-03-31 23:40:24 +0800

    双向链表

--- 20211231单链表02/TestDemo2.java
+++ 20211231单链表02/TestDemo2.java
@@ -18,4 +18,21 @@ public class TestDemo2 {
         doubleList.addLast(5);
         doubleList.display();
     }
+
+    public static void main(String[] args) {
+        DoubleList doubleList = new DoubleList();
+        doubleList.addLast(1);
+        doubleList.addLast(2);
+        doubleList.addLast(3);
+        doubleList.addLast(4);
+        doubleList.addLast(5);
+        doubleList.display();
+        doubleList.addIindex(0,99);
+        doubleList.addIindex(6,88);
+        doubleList.addIindex(3,66);
+        doubleList.display();
+        System.out.println(doubleList.contains(88));
+        doubleList.remove(3);
+        doubleList.display();
+    }
 }
