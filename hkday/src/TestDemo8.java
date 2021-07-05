7ff5d7745bf6dc5f76c36f563e1a4b19737d419e
xiong717 <74310990+xiong717@users.noreply.github.com>
2021-07-05 22:53:42 +0800

    Update TestDemo8.java

--- hkday/src/TestDemo8.java
+++ hkday/src/TestDemo8.java
@@ -4,12 +4,9 @@ import java.util.Scanner;
 public class TestDemo8 {
     
     public static void main1(String[] args) {
-        
-        
         B b = new B();
         int a =b.getValue();
         System.out.println(a);
-        
     }
     static class A{
         protected int value;
