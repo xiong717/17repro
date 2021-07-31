483fc4a8de62cebc6015f038a95e68d6353624ca
xiong717 <1304158615@qq.com>
2021-07-31 23:07:12 +0800

    每日一题3

--- hkday/src/TestDemo3.java
+++ hkday/src/TestDemo3.java
@@ -30,37 +30,74 @@ public class TestDemo3 extends Person{
             new Derived("C");
         }
     }
-    public static void reverse(char[] ch,int start,int end){
-        while(start < end){
-            char tmp = ch[start];
-            ch[start] = ch[end];
-            ch[end] = tmp;
-            start++;
-            end--;
-        }
-    }
-    public static void main(String[] args){
+
+    //字符串中找出连续最长的字符串
+    public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         String str = scan.nextLine();
-        char[] ch = str.toCharArray();
-        int len = ch.length;
-        //1.逆置整体
-        reverse(ch,0,len-1);
-        int i =0;//遍历字符数组
-        while(i < len){
-            int j = i;
-            while(j<len && ch[j] != ' '){
-                j++;
-            }
-            if(j < len){
-                reverse(ch,i,j-1);
-                i = j+1;
-            }else{
-                reverse(ch,i,j-1);
-                i = j;
+
+        String cur = "";
+        String ret = "";
+        int i=0;
+        for(;i<str.length();i++)
+        if (i == str.length() && cur.length()>ret.length() ){
+            ret = cur;
+        }
+        System.out.print(ret);
+    }
+
+    //数组中有一个数字出现的次数超过数组长度的一半，请找出这个数字。
+    //思路一 ：先给数组排序，找到中间的数字x 遍历数组看x出现了多少次 超过数组长度的一半 则是
+    public int MoreThanHalfNum_Solution(int [] array) {
+        if(array.length == 0||array == null){
+            return 0;
+        }
+        Arrays.sort(array);
+        int mid = array.length/2;
+        int count=0;
+        for(int j= 0;j< array.length;j++){
+            if(array[j]==array[mid]){
+                count++;
             }
         }
-        String s = new String(ch);
-        System.out.println(s);
+        if(count>mid){
+            return array[mid];
+        }else{
+            return 0;
+        }
+    }
+
+    //思路二：找 众数
+    //先设置一个 result和times 然后比较两个数是否相等， 不相等就消去这两个数 最坏情况下，每消去一个众数和非众数，那么如果存在众数的情况下，最后留下的数 就是众数
+    //最后验证一下 遍历数组 看最后的result出现了几次
+    public int MoreThanHalfNum_Solution2(int [] array) {
+        if(array == null || array.length == 0){
+            return 0;
+        }
+        int result = array[0];
+        int times = 1;
+        int len = array.length;
+        for(int i =1; i<len;i++){
+            if(times != 0){
+                if(result != array[i]){
+                    --times;
+                }else{
+                    ++times;
+                }
+            }else{
+                times = 1;
+                result = array[i];
+            }
+        }
+        int count =0;
+        for(int i =0;i<len;i++){
+            if(result == array[i]){
+                count++;
+            }
+        }
+        if(count > len/2){
+            return result;
+        }
+        return 0;
     }
 }
