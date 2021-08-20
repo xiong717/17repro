f388431efe8590fd098df640c78a2679fe07459e
xiong717 <1304158615@qq.com>
2021-08-20 23:39:12 +0800

    末尾0的个数-数字颠倒

--- hkday/src/TestDemo9.java
+++ hkday/src/TestDemo9.java
@@ -16,7 +16,7 @@ public class TestDemo9 {
             System.out.print(al.get(i));
         }
     }
-    public static void main(String[] args) {
+    public static void main2(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         System.out.println(reverse(n));
@@ -33,4 +33,83 @@ public class TestDemo9 {
         // toString() -- 返回此序列中数据的字符串表示形式。
         return sb.toString();
     }
+
+    public static void main3(String[] args) {
+        String x = "fmn";
+        x.toUpperCase();
+        String y = x.replace('f','F');
+        y = y+"wxy";
+        System.out.println(y);
+    }
+    static class Person {
+        String name = "No name";
+        public Person(String nm) {
+            name = nm;
+        }
+
+    }
+    static class Employee extends Person {
+        String empID = "0000";
+        public Employee(String id) {
+            super(id);//注意要有super
+            empID = id;
+        }
+    }
+
+    public static void main4(String args[]) {
+            Employee e = new Employee("123");
+            System.out.println(e.empID);
+    }
+
+
+
+        private static void testMethod(){
+            System.out.println("testMethod");
+        }
+        public static void main5(String[] args) {
+            TestDemo9.testMethod();
+        }
+
+        //末尾0 的个数
+    public static void main6(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int num = scan.nextInt();
+        int count =0;
+        for( int i=1; i<=num;i++){
+            int m =i;
+            while(m%5 == 0){
+                count++;
+                m=m/5;
+            }
+        }
+        System.out.println(count);
+    }
+
+        public static void main7(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+            int num = scanner.nextInt();
+            int count=0;
+            while (num>0) {
+                count+=num/5;
+                num/=5;
+            }
+            System.out.println(count);
+        }
+
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        int num = scan.nextInt();
+        String s = revers(num);
+        System.out.print(s);
+    }
+    public static String revers(int num){
+        String s = String.valueOf(num);
+        StringBuffer sb = new StringBuffer();
+        for(int i = s.length()-1;i>=0;i--){
+            sb.append(s.charAt(i));
+        }
+        return sb.toString();
+    }
+
 }
+
